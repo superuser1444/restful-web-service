36f10ebb8e2de31d5db801406e093e3c36662380
DaeKwon Hwang <superuser1444@gmail.com>
2021-08-18 20:50:32 +0900

    섹션 3. RESTful Service 기능 확장 (~프로그래밍으로 제어하는 Filtering 방법 - 전체 사용자 조회)

--- src/main/java/com/example/restfulwebservice/user/AdminUserController.java
+++ src/main/java/com/example/restfulwebservice/user/AdminUserController.java
@@ -18,8 +18,21 @@ public class AdminUserController {
     }
 
     @GetMapping("/users") // <- users: endpoint
-    public List<User> retrieveAllUsers() {
-        return service.findAll();
+    public MappingJacksonValue retrieveAllUsers() {
+        List<User> users = service.findAll();
+
+        // SimpleBeanPropertyFilter : Bean의 Property를 제어
+        //포함시키고자 하는 필터 값
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate", "ssn");
+        // id : 필터가 어떠한 Bean을 대상으로 사용될지 필터의 이름을 적어줘야 하는데 이에 해당
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        // 클라이언트에 반환하는 Object 타입이 User가 아닌 필터를 적용할 수 있는 MappingJacksonValue 타입으로 변경
+        MappingJacksonValue mapping = new MappingJacksonValue(users);
+        mapping.setFilters(filters);
+
+        return mapping;
     }
 
     // GET /users/1 or /users/10 -> String
