3491bfe0ec31ce3481b4621ece9952723c415f10
DaeKwon Hwang <superuser1444@gmail.com>
2021-08-21 18:56:14 +0900

    섹션4. Spring Boot API 사용 (~HAL Browser를 이용한 HATEOAS 기능 구현)

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,6 +1,7 @@
 package com.example.restfulwebservice.user;
 
-import com.fasterxml.jackson.annotation.JsonFilter;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,19 +14,24 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 //@JsonIgnoreProperties(value={"password", "ssn"})
-@JsonFilter("UserInfo") //UserInfo : 임의의 이름
+//@JsonFilter("UserInfo") //UserInfo : 임의의 이름
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
     private Integer id;
 
     @Size(min = 2, message = "Name은 2글자 이상 입력해 주세요.")
+    @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
     private String name;
 
     @Past // 미래의 date를 사용할 수  없고, 과거의 date만 사용가능
+    @ApiModelProperty(notes = "사용자 등록일을 입력해 주세요.")
     private Date joinDate;
 
     // @JsonIgnore
+    @ApiModelProperty(notes = "사용자 패스워드를 입력해 주세요.")
     private String password;
 
     // @JsonIgnore
+    @ApiModelProperty(notes = "사용자 주민번호를 입력해 주세요.")
     private String ssn;
 }
