8942474f97a4c52e7e2c4e5e11ee7439d5220bd8
DaeKwon Hwang <superuser1444@gmail.com>
2021-08-21 17:29:28 +0900

    섹션4. Spring Boot API 사용 (~REST API Documentation을 위한 Swagger 사용)

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,12 +1,21 @@
 package com.example.restfulwebservice.user;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
@@ -23,13 +32,29 @@ public class UserController {
 
     // GET /users/1 or /users/10 -> String
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public MappingJacksonValue retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
 
         if (user == null) {
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
         }
-        return user;
+
+        // HATEOAS
+        Resource<User> resource = new Resource<>(user);
+        // 어떠한 링크를 추가시킬지 지정
+        ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        resource.add(linkTo.withRel("all-users"));
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate");
+        // id : 필터가 어떠한 Bean을 대상으로 사용될지 필터의 이름을 적어줘야 하는데 이에 해당
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        // 클라이언트에 반환하는 Object 타입이 User가 아닌 필터를 적용할 수 있는 MappingJacksonValue 타입으로 변경
+        MappingJacksonValue mapping = new MappingJacksonValue(resource);
+        mapping.setFilters(filters);
+
+        return mapping;
     }
 
     @PostMapping("/users")
